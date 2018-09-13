5e36b9f1085d88984914a606cd23afb82932972e
Chris Haumesser <1550854+wryfi@users.noreply.github.com>
2018-09-13 08:30:30 -0700

    add prometheus instrumentation

--- online/app.py
+++ online/app.py
@@ -4,15 +4,22 @@ application with Prometheus.
 
 """
 
+import random
+import time
+from uuid import uuid4
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.exc import DataError
+from sqlalchemy.orm import exc
 from sqlalchemy.dialects.postgresql import UUID
-from uuid import uuid4
 
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
-from prometheus_client import make_wsgi_app
+
+from prometheus_client import Counter, Gauge, Histogram, make_wsgi_app
 
 
 app = Flask(__name__)
@@ -40,22 +47,35 @@ class Widget(db.Model):
     waggles = db.Column(db.Integer)
 
 
+# prometheus instruments
+WIDGET_LIST_TIME = Histogram('widget_get_seconds', 'Time spent getting a widget')
+WIDGET_REQUEST_ERRORS = Counter('widget_request_errors', 'Errors processing widget requests', ['method', 'endpoint'])
+WIDGET_COUNT = Gauge('widget_count', 'Number of widgets in the database')
+WIDGET_COUNT.set_function(lambda: db.session.query(Widget).count())
+
+
 class WidgetList(Resource):
     """
     API resource to list widgets (get) or create a new widget (post)
 
     """
+    @WIDGET_LIST_TIME.time()
     def get(self):
         widgets = db.session.query(Widget).all()
         serialized = [
             {'id': str(widget.id), 'name': widget.name, 'wongles': widget.wongles, 'waggles': widget.waggles}
             for widget in widgets
         ]
+        time.sleep(random.uniform(0.005, 10))
         return serialized
 
     def post(self):
         data = request.get_json()
-        widget = Widget(**data)
+        try:
+            widget = Widget(**data)
+        except TypeError:
+            WIDGET_REQUEST_ERRORS.labels(method='post', endpoint='/widgets').inc(1)
+            return 'invalid parameters', 400
         db.session.add(widget)
         db.session.commit()
         return 'created', 201
@@ -67,7 +87,11 @@ class WidgetDetail(Resource):
 
     """
     def delete(self, widget_id):
-        widget = db.session.query(Widget).filter(Widget.id == widget_id).one()
+        try:
+            widget = db.session.query(Widget).filter(Widget.id == widget_id).one()
+        except (DataError, exc.NoResultFound, exc.MultipleResultsFound):
+            WIDGET_REQUEST_ERRORS.labels(method='delete', endpoint='/widgets/{}'.format(widget_id)).inc(1)
+            return 'widget not found', 404
         db.session.delete(widget)
         db.session.commit()
         return 'deleted', 200
